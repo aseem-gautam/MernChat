88051c87dcaa41d68c2a69f399d6cb5413894931
aseem-gautam <gautamaseem3@gmail.com>
2025-08-29 14:47:25 +0545

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+import { connectDB } from "./lib/db.js";
+
+
+dotenv.config();
+const PORT= process.env.PORT;
+
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+
+connectDB().then(() => {
+    server.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+});
+});
+
+
